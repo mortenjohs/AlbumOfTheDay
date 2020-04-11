dc1bda9927a760c565920752b161f5676ae0d8fd
Morten Ervik <morten@ervik.fr>
2020-04-11 15:49:59 +0200

    Now we generate a random suggestion as well.

--- generate_files.rb
+++ generate_files.rb
@@ -11,27 +11,42 @@ require "fileutils"
 puts Time.now # for the log file...
 
 config_file   = "./config.yml"
+config_override_file = "./config_override.yml"
 
 config = YAML.load(File.open(config_file).read)
+config = config.merge(YAML.load(File.open(config_override_file).read)) if File.exist?(config_override_file)
+
+puts config
 
 # make sure folders exist:
 [config['cache'], config['rss_dir'], config['html_dir']].each { |dirname| FileUtils.mkdir_p(dirname) unless File.directory?(dirname) }
 
 base_url = config["songlink_api"] + "?"
-
 config["url_parameters"].each { |k,v| base_url+="#{URI::encode(k)}=#{URI::encode(v)}&" } # leave trailing &!
 
 all_albums = {}
 
-CSV.read(config["csv_file"], :headers => true).each do |row|
-  album = row.to_h
-  file_name = "#{config['cache']}/#{album['date']}.json"
+def attach_songlink_data!(album, data)
+  ## thumbnail
+  album["thumbnail"] = data["entitiesByUniqueId"][data["entityUniqueId"]]["thumbnailUrl"]
+  ## find links
+  album["providers"] = {}
+  album["providers"]["bandcamp"] = album["bandcamp"] unless album["bandcamp"].nil?
+  data["linksByPlatform"].each do |k,v| 
+      album["providers"][k]=v["url"]
+  end
+end
+
+def get_songlink_info(album, base_url, cache='./cache/')
+  spotify_id = album['spotify-app'].split(":")[-1]
+  # file_name = "#{cache}/#{album['date']}.json"
+  file_name = "#{cache}/#{album['date']}.json"
   data = {}
   if File.exist?(file_name)
     data = JSON.parse(File.open(file_name).read)
     # check that the spotify UUID hasn't changed -- and clear if so
     data = {} if data["entityUniqueId"].split("::")[-1]!=album['spotify-app'].split(":")[-1]
-  end 
+  end
   if data.empty?
     puts "Downloading: #{album['artist']} - #{album['album']}"
     url = base_url + "url=#{album['spotify-app']}"
@@ -40,16 +55,17 @@ CSV.read(config["csv_file"], :headers => true).each do |row|
       File.open(file_name, "w") {|file| file << JSON.pretty_generate(data) }      
     end
   end
-  ## thumbnail
-  album["thumbnail"] = data["entitiesByUniqueId"][data["entityUniqueId"]]["thumbnailUrl"]
+  data
+end
+
+
+CSV.read(config["csv_file"], :headers => true).each do |row|
+  album = row.to_h
   ## date
   album["date_obj"]  = Date.strptime(album["date"])
-  ## find links
-  album["providers"] = {}
-  album["providers"]["bandcamp"] = album["bandcamp"] unless album["bandcamp"].nil?
-  data["linksByPlatform"].each do |k,v| 
-      album["providers"][k]=v["url"]
-  end
+
+  data = get_songlink_info(album, base_url, config['cache'])
+  attach_songlink_data!(album, data)
 
   all_albums[album["date"]] = album
 end
@@ -76,6 +92,54 @@ def rss_generator(provider, data, author, base_url)
     end
   end
   rss
+end
+
+def get_album_by_provider_ref(ref, base_url = "https://api.song.link/v1-alpha.1/links?", cache = './cache')
+  # override cache with cache = nil
+  url = base_url + "url=#{ref}"
+  data = {}
+  open(url) do |f|
+    data = JSON.parse(f.read)      
+  end
+  data
+end
+
+def get_songlink_album(artist, album)
+  # use itunes to find an url -- first 
+  url = "https://itunes.apple.com/search?entity=album&term="
+  url += URI::encode("\"#{artist}\"-\"#{album}\"")
+  album = {}
+  open(url) do |f| 
+    data = JSON.parse(f.read) 
+    if data['resultCount'] > 0
+      itunes_url = data['results'].first['collectionViewUrl']
+      album = get_album_by_provider_ref(itunes_url)
+    end
+  end
+  album
+end
+
+def get_similar_artists_by_name_lastfm(artist,api_key,limit=10,base_url="http://ws.audioscrobbler.com/2.0/")
+  url = "#{base_url}?method=artist.getsimilar&artist=#{artist}&api_key=#{api_key}&format=json&limit=#{limit}"  
+  data = {}
+  open(url) do |f|
+    data = JSON.parse(f.read)
+  end
+  data['similarartists']['artist']
+end
+
+def get_albums_from_artist_by_mbid_lastfm(mbid,api_key,limit=10,base_url="http://ws.audioscrobbler.com/2.0/")
+  url = "#{base_url}?method=artist.getTopAlbums&mbid=#{mbid}&api_key=#{api_key}&format=json&limit=#{limit}"  
+  data = {}
+  open(url) { |f| data = JSON.parse(f.read) }
+  data['topalbums']['album']
+end
+
+def get_random_album_from_similar_artist(artist, api_key)
+  artist      = get_similar_artists_by_name_lastfm(artist,api_key).sample
+  mbid        = artist['mbid']
+  album       = get_albums_from_artist_by_mbid_lastfm(mbid,api_key).sample
+  get_songlink_album(artist['name'], album['name']) 
 end
 
 providers  = []
@@ -109,4 +173,29 @@ end
 
 File.open("#{config['html_dir']}/index.html", "w") do |file|
   file << Tilt.new('views/index.erb').render(self, :today => Date.today)
+end
+
+unless config['lastfm']['api_key'].nil?
+  File.open("#{config['html_dir']}/random.html", "w") do |file|
+    artist = all_albums.values.map { |e| e['artist']}.sample
+    data = {}
+    tries = 0
+    threshold = 5
+    while tries < threshold && data.empty?
+      data = get_random_album_from_similar_artist(artist, config['lastfm']['api_key']) 
+      tries += 1
+    end
+    unless data.empty?
+      album = {}
+      album['date_obj'] = Date.today-1
+      album['artist']   = data["entitiesByUniqueId"][data["entityUniqueId"]]["artistName"]
+      album['album']    = data["entitiesByUniqueId"][data["entityUniqueId"]]["title"]
+      album['date']     = Date.today.to_s
+      album['random']   = true
+      album['comment']  = "Random suggestion similar to: #{artist}."
+      attach_songlink_data!(album, data)
+      file << album_template.render(self, :album => album, :first_date => first_date, :providers => providers )
+      puts "Random album: #{data["entitiesByUniqueId"][data["entityUniqueId"]]["artistName"]} - #{data["entitiesByUniqueId"][data["entityUniqueId"]]["title"]} (Based on #{artist}.)"
+    end
+  end
 end
